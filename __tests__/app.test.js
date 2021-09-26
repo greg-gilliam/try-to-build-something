431d2a1b6dda1df4c9879a1e5f5f7fef13100e60
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-09-25 18:04:34 -0700

    still failing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,33 +2,25 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const insult = require('../lib/models/insult');
 
 jest.mock('twilio', () => () => ({
   messages: {
     create: jest.fn()
-  }
+  },
 }));
 
-describe('insult demo routes', () => {
+describe('insult routes', () => {
   beforeEach(() => {
     return setup(pool);
-  })
-}
-
-//   afterAll(() => {
-//     pool.end();
-//   });
-// });
-
-  it('creates a new insult in db and sends a text', () => {
-    return request(app)
-      .post('/api/v1/quotes')
-      .send({ insult })
-      .then(res => {
-        expect(res.body).toEqual.any(string)({
-          id: '1',
-          quotes: ''
-        });
-      });
   });
 
+  it('creates a new insult in our database and sends a text message', () => {
+    return request(app)
+      .post('/api/v1/quotes')
+      .send({ quotes: 'blah' })
+      .then(res => {
+        expect(res.body).toEqual.any(String);
+      });
+  });
+});
