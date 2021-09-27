bff731dae07536e759d864bd03bad40925b97c36
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-09-27 15:03:03 -0700

    passing put test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,13 @@
 const pool = require('../lib/utils/pool');
+const twilio = require('../lib/utils/twilio');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const insult = require('../lib/models/insult');
+// const Insult = require('../lib/models/Insult.js');
 
 jest.mock('twilio', () => () => ({
   messages: {
-    create: jest.fn()
+    create: jest.fn(),
   },
 }));
 
@@ -15,12 +16,21 @@ describe('insult routes', () => {
     return setup(pool);
   });
 
+  beforeEach(() => {
+    return request(app).post('/api/v1/insults').send({
+      quotes: 'something',
+    });
+  });
+
   it('creates a new insult in our database and sends a text message', () => {
     return request(app)
-      .post('/api/v1/quotes')
-      .send({ quotes: 'blah' })
+      .post('/api/v1/insults')
+      .send({ quotes: 'something' })
       .then(res => {
-        expect(res.body).toEqual.any(String);
+        expect(res.body).toEqual({
+          id: '2',
+          quotes: 'something',
+        });
       });
   });
 });
